cd9cc4cf724844439b7b47ad91b304e919b1bd53
Fabio Dos Santos <fabio.dossantos@mirgor.com.ar>
2022-09-27 12:47:16 -0400

    FTP Dpwnload

---
+++ FtpClientCs/FtpService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FtpClient
+{
+    public class FtpService
+    {
+        public string DownloadFile(string requestUriString, string userName, string password)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(requestUriString);
+            request.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.CacheIfAvailable);
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.Credentials = new NetworkCredential(userName, password);
+            request.KeepAlive = false;
+            request.UseBinary = true;
+            request.UsePassive = true;
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            string content = reader.ReadToEnd();
+            response.Close();
+            reader.Close();
+            return content;
+        }
+    }
+}
--- FtpClientCs/Program.cs
+++ FtpClientCs/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FtpClient;
+using System.Net;
+using System.Net.Cache;
+
+try
+{
+    string requestUriString = "ftp://ftp.dlptest.com/Tempe_2170-S8P20120083-DIReading-2022-09-27T08-10-00-0700-1min.json";
+    string userName = "dlpuser";
+    string password = "rNrKYTX9g7z3RgJRmxWuGHbeu";
+
+    FtpService ftpService = new FtpService();
+    string response = ftpService.DownloadFile(requestUriString, userName, password);
+
+    Console.WriteLine(response);
+    //Console.WriteLine("Download Complete", response.StatusDescription);
+}
+catch (WebException e)
+{
+    Console.WriteLine(e.Message.ToString());
+    String status = ((FtpWebResponse)e.Response).StatusDescription;
+    Console.WriteLine(status);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message.ToString());
+}
