056b8c8486e48812ef66bdba8e0e6ea6e9b56b95
Fabio Dos Santos <Fabio.DosSantos@mirgor.com.ar>
2023-01-24 16:05:34 -0300

    FTP Web Request Download stream

--- FtpClientCs/FtpService.cs
+++ FtpClientCs/FtpService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentFTP;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -6,27 +7,35 @@ using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FtpClient
+namespace FtpClientCs
 {
     public class FtpService
     {
-        public string DownloadFile(string requestUriString, string userName, string password)
+        public string UserName;
+        public string Password;
+        public FtpService(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+        public Stream DownloadStream(string requestUriString)
         {
             FtpWebRequest request = (FtpWebRequest)WebRequest.Create(requestUriString);
             request.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.CacheIfAvailable);
             request.Method = WebRequestMethods.Ftp.DownloadFile;
-            request.Credentials = new NetworkCredential(userName, password);
+            request.Credentials = new NetworkCredential(UserName, Password);
             request.KeepAlive = false;
             request.UseBinary = true;
             request.UsePassive = true;
 
             FtpWebResponse response = (FtpWebResponse)request.GetResponse();
             Stream responseStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(responseStream);
-            string content = reader.ReadToEnd();
-            response.Close();
-            reader.Close();
-            return content;
+            //StreamReader reader = new StreamReader(responseStream);
+            //string content = reader.ReadToEnd();
+            //response.Close();
+            //reader.Close();
+            //return content;
+            return responseStream;
         }
     }
 }
---
+++ FtpClientCs/FtpWebRequestDemo.cs
@@ -0,0 +1,42 @@
+﻿using FtpClientCs;
+using System.Net;
+using System.Net.Cache;
+using System.Text;
+
+namespace FtpClientCs
+{
+    static class FtpWebRequestDemo
+    {
+        public static void Demo()
+        {
+            try
+            {
+                string requestUriString = "ftp://ftp.dlptest.com/23012023.csv";
+                string userName = "dlpuser";
+                string password = "rNrKYTX9g7z3RgJRmxWuGHbeu";
+
+                FtpService ftpService = new FtpService(userName, password);
+                using (Stream response = ftpService.DownloadStream(requestUriString))
+                {
+                    //response.Position = 0;
+                    StreamReader reader = new StreamReader(response);
+                    string content = reader.ReadToEnd();
+                    Console.WriteLine(content);
+                    reader.Close();
+                }
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                String status = ((FtpWebResponse)e.Response).StatusDescription;
+                Console.WriteLine(status);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+        }
+
+    }
+}
